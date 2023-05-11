eb58acbd875157efc00c509924631c82860ed59f
James G Willmore <jim@localhost.localdomain>
2023-05-11 10:28:01 -0400

    Initial commit.

---
+++ src/test/java/net/ljcomputing/htmltopdf/HtmltopdfApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ljcomputing.htmltopdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmltopdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
